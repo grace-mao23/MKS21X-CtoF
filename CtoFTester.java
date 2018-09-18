3d0a1dd9e7f839fdef7cd8510583f3c587c6044a
grace-mao23 <gmao00@stuy.edu>
2018-09-18 19:19:53 -0400

    Wrote out functions

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,1 +1,7 @@
-
+public class HelloWorld{
+    public static double celsiusToFahrenheit(int temp){
+	return temp * 9.0 / 5 + 32;
+    }
+    public static double fahrenheitToCelsius(int temp){
+	return (temp - 32) * 5.0 / 9
+}
